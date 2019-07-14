13c687e0f2e31998a9cee1ef48420cd9ac79fc83
Alex Curci <alex.curci@gmail.com.com>
2019-07-14 05:56:33 +0000

    Add http basic auth to admin pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # HTTP Basic Authentication
+  # include HttpAuthConcern
+
+
   private
 
   def cart
